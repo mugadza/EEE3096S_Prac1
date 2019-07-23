c4973c9a59854479b82894638f642c93a00d1fa1
mugadza <mgdtat002@myuct.ac.za>
2019-07-23 23:58:52 +0200

    added button control to count up and down

--- Counter.py
+++ Counter.py
@@ -1,70 +1,77 @@
 import RPi.GPIO as GPIO     # importing GPIO library
 import time                 # importing time library for delay
 
+
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)     # enable BOARD pin numberings
 GPIO.setup(11,GPIO.OUT)      # Set pin 11 as output
 GPIO.setup(13,GPIO.OUT)
 GPIO.setup(15,GPIO.OUT)
-GPIO.setup(7, GPIO.IN)
+GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
+counter = 0
 
 def main():
-    #toggle()
-    countUpKhomo()
+    GPIO.add_event_detect(7, GPIO.RISING, callback=handler,bouncetime=400)
+    GPIO.add_event_detect(12, GPIO.RISING, callback=handler,bouncetime=400)
+    while True:
+        time.sleep(0)
     
-def toggle():
-    onOff = 1
 
-    alreadyPressed = 0
-    while(True):
-        GPIO.output(11, onOff) # set LED state
-         
-        if (GPIO.input(7) == True):         # Check if switch is pressed
-            if(alreadyPressed == 1):        # If its already pressed, skip everything below (i.e continue)
-                continue
-            
-            alreadyPressed = 1              # If not, then change the state to pressed
-
-            if(onOff == 1):
-                onOff = 0
-            else:
-                onOff = 1
-        else:
-            alreadyPressed = 0
-            
-        time.sleep(0.010)
-
-#def countUp():
-#    for x in range (0,8):
-#        BinString = str(bin(x))
-#        BinList = list(BinString)
-#        for i in range(2, len(BinList)):
-#            GPIO.output(11, i)
-#            GPIO.output(13, onOff)
-#            GPIO.output(15, onOff)
-              
+def handler(pin):
+    global counter
+    print("=====================================")
+    if pin==7:
+        print("counting up")
+        counter += 1
+        if counter == 8:
+            counter = 0
+        countUp(counter)
         
-def countUpKhomo():
-    for x in range (0,7):         #increament decimal
-        BinString = str(bin(x))     #covert decimal to binary
-        InitList = [0,0]                #list to keep Binlist big enough
-        TempList = list(BinString)    #binary number split to list
-        BinList = TempList + InitList  #concatinate lists to ensure index is 3 or bigger
+    if pin==12:
+        print("counting down")
+        counter -= 1
+        if counter == -1:
+            counter = 7
+        countDown(counter)
+   
+    return
         
-        
-        GPIO.output(11, int(BinList[2]))  #set state of led1
-        GPIO.output(13, int(BinList[3]))  #set state of led2
-        GPIO.output(15, int(BinList[4]))  #set state of led3
-        
-        time.sleep(0.5)  #delay to make changes visible
+def countUp(x):
+    binaryList = makeThreeBitBinary(x)
     
+    GPIO.output(11, int(binaryList[0]))  #set state of led1
+    GPIO.output(13, int(binaryList[1]))  #set state of led2
+    GPIO.output(15, int(binaryList[2]))  #set state of led3
+    print(binaryList)
+    time.sleep(1)  #delay to make changes visible
+
+def countDown(x):
+    binaryList = makeThreeBitBinary(x)
+        
+    GPIO.output(11, int(binaryList[0]))  #set state of led1
+    GPIO.output(13, int(binaryList[1]))  #set state of led2
+    GPIO.output(15, int(binaryList[2]))  #set state of led3
+    print(binaryList)
+    time.sleep(1)  #delay to make changes visible
+        
+def makeThreeBitBinary(n):
+    binaryString = str(bin(n))[2:]
+    bitList = list(binaryString)
+
+    while(len(bitList) < 3):
+        bitList = ['0'] + bitList
+
+    return bitList
+
     
 if __name__ == "__main__":
     # Make sure the GPIO is stopped correctly
     try:
-        while True:
-            main()
+        #while True:
+        main()
+            
     except KeyboardInterrupt:
         print("Exiting gracefully")
         # Turn off your GPIOs here
